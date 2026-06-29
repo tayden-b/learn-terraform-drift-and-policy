67a75e11fc31e8a2c335f373742c9e93667616d2
tayden-b <tayden.barretto@hashicorp.com>
2026-06-29 14:42:29 -0500

    Update description for bastion_instance_type variable
    Added additional description for bastion instance type variable.

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 }
 
 variable "bastion_instance_type" {
-  description = "EC2 instance type to use for bastion instance"
+  description = "EC2 instance type to use for bastion instance, additional description added here"
   type        = string
 }
 
